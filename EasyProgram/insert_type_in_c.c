c20c66252cfc03f84a43e28bf0beff70fa0a72cf
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-07-30 17:42:50 +0300

    Added new file insrt_type_in_c

---
+++ EasyProgram/insert_type_in_c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("sizeof(char) = %ld\n", sizeof(char));
+	printf("sizeof(short) = %ld\n", sizeof(short));
+	printf("sizeof(int) = %ld\n", sizeof(int));
+	printf("sizeof(long) = %ld\n", sizeof(long));
+	printf("sizeof(long long) = %ld\n", sizeof(long long));
+	printf("sizeof(float) = %ld\n", sizeof(float));
+	printf("sizeof(double) = %ld\n", sizeof(double));
+	printf("sizeof(long double) = %ld\n", sizeof(long double));
+	return 0;
+
+}
