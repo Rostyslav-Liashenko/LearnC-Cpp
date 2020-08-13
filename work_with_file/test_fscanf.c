21795ebd10aec51138918f21636b666a5a1dc1a6
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-08-13 20:16:17 +0300

    test function fscanf

--- work_with_file/test_fscanf.c
+++ work_with_file/test_fscanf.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /* it's file not complete program, it's example code with fscanf */
 void int_file_sum(FILE *f, int *sum, int *count)
 {
@@ -7,12 +8,18 @@ void int_file_sum(FILE *f, int *sum, int *count)
     while(fscanf(f, "%d", &n) == 1)
     {
         *sum += n;
-        *count++;
+        (*count)++;
     }
 }
 
 int main(void)
 {
-
+    FILE *f;
+    int sum = 0, count = 0;
+    f = fopen("file1.txt", "r");
+    int_file_sum(f,&sum,&count);
+    printf("sum = %d\n",sum);
+    printf("count = %d\n",count);
+    fclose(f);
     return 0;
 }
