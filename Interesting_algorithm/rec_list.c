cc4d2b70d922c8e536b8021ff8b7058ccef323d2
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-08-21 20:47:11 +0300

    added recursion solved

---
+++ Interesting_algorithm/rec_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct item
+{
+    int data;
+    struct item *next;
+};
+
+struct item *int_array_to_list(int *arr, int len)
+{
+    struct item *tmp;
+    if (!len)
+        return NULL;
+    tmp = malloc(sizeof(struct item));
+    tmp->data = *arr;
+    tmp->next = int_array_to_list(arr+1,len - 1);
+    return tmp;
+}
+
+int main(void)
+{
+
+    return 0;
+}
