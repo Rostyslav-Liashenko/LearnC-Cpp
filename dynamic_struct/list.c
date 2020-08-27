67706e1822738f02b02640b52a942aceac9ac933
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-08-27 17:57:56 +0300

    Write new functional: create new functional and show list

--- dynamic_struct/list.c
+++ dynamic_struct/list.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <stdlib.h> /* rand() */
+struct item 
+{
+    int data;
+    struct item *next;
+};
+
+struct item *create_random_list(int count_node)
+{
+    struct item *head = NULL, *tmp;
+    while (count_node)
+    {
+        tmp = malloc(sizeof(struct item));
+        tmp->data = rand() % 10 + 1;
+        tmp->next = head;
+        head = tmp;
+        count_node--;
+    }
+    return head;
+}
+
+void show_list(struct item *head)
+{
+    for(; head; head = head->next)
+        printf("%d -> ",head->data);
+    putchar(10);
+}
 
 int main(void)
-{
+{    
+    int len;
+    printf("Input length of  list: ");
+    scanf("%d",&len);
 
+    struct item *head = create_random_list(len);
+    show_list(head);
     return 0;
 }
