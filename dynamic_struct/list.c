93679bd516f1178b57b8868f6251bf904875a72e
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-08-28 11:32:27 +0300

    Added function for delete only negative nodes of list

--- dynamic_struct/list.c
+++ dynamic_struct/list.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h> /* rand() */
+
 struct item 
 {
     int data;
@@ -13,7 +14,7 @@ struct item *create_random_list(int count_node)
     while (count_node)
     {
         tmp = malloc(sizeof(struct item));
-        tmp->data = count_node;
+        tmp->data = rand() % 10 - 5;
         tmp->next = head;
         head = tmp;
         count_node--;
@@ -49,6 +50,23 @@ void delete_list(struct item *head)
     }
 }
 
+void delete_negatives_from_int_list(struct item **pcur)
+{
+    while(*pcur)
+    {
+        if ((*pcur)->data < 0)
+        {
+            struct item *tmp = *pcur;
+            *pcur = (*pcur)->next;
+            free(tmp);
+        }
+        else
+        {
+            pcur = &(*pcur)->next;
+        }
+    }
+}
+
 int main(void)
 {    
     int len;
@@ -58,6 +76,8 @@ int main(void)
     struct item *head = create_random_list(len);
     show_list(head);
     printf("Summa nodes = %d\n",summa_nodes_list(head));
-    delete_list(head);
+    /*delete_list(head);*/
+    delete_negatives_from_int_list(&head);
+    show_list(head);
     return 0;
 }
