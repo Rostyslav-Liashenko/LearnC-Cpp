9820bb1f7cc4edac61c6441f0ca8c145988b7055
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-09-05 16:21:41 +0300

    Added constant for work with matrix

--- Matrix/test_matrix.c
+++ Matrix/test_matrix.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define N 4
+#define M 3
 
-void fill_matrix(int (*a)[4], int rows) /* a = pointer begin matrix, 4 = columns rows = rows */
+void fill_matrix(int (*a)[N], int rows) /* a = pointer begin matrix, 4 = columns rows = rows */
 {
     srand(time(NULL));
     int i,j;
     for (i = 0; i < rows; i++)
-        for (j = 0; j < 4; j++)
+        for (j = 0; j < N; j++)
             a[i][j] = rand() % 20;
 }
 
@@ -17,7 +19,7 @@ void print_matrix(int (*a)[4], int rows)
     int i,j;
     for (i = 0; i < rows; i++)
     {
-        for (j = 0; j < 4; j++)
+        for (j = 0; j < N; j++)
             printf("%4d",a[i][j]);
         printf("\n");
     }
@@ -26,13 +28,13 @@ void print_matrix(int (*a)[4], int rows)
 int main(void)
 {
     int (*p) [4];
-    int matr[3][4];
+    int matr[M][N];
 
     p = matr; /* pointer for array */
 
     int m[3][4] = {{0,0,0}, {0,1,2,3}, {0,2,4,6}}; /* init array */
 
-   fill_matrix(matr,3);
-   print_matrix(matr,3);     
+   fill_matrix(matr,M);
+   print_matrix(matr,M);     
     return 0;
 }
