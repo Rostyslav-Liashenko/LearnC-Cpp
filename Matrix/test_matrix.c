1386dd51aa6fe89fff19ea1d21f76bf631ee24fe
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-09-05 15:43:53 +0300

    Create ponter for array

--- Matrix/test_matrix.c
+++ Matrix/test_matrix.c
@@ -3,7 +3,11 @@
 int main(void)
 {
     int i,j;
+
+    int (*p) [4];
     int matr[3][4];
+
+    p = matr; /* pointer for array */
 
     int m[3][4] = {{0,0,0}, {0,1,2,3}, {0,2,4,6}}; 
     for (i = 0; i < 3; i++)
