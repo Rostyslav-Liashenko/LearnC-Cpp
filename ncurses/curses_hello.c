645af0e7047cdb081084b40b05b0d41e0bfd2a2b
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-09-12 16:02:17 +0300

    Learn library ncurses

---
+++ ncurses/curses_hello.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ncurses.h>
+#include <unistd.h>
+
+const char message[] = "Hello, world";
+
+enum {delay_duration = 5};
+
+int main(void)
+{
+    int row,col;
+    initscr(); /* link library */
+    getmaxyx(stdscr,row,col); /* get size terminal */
+    move(row/2,(col - sizeof(message) -1) / 2);
+    addstr(message);
+    curs_set(0);
+    refresh();
+    sleep(delay_duration);
+    endwin();
+    return 0;
+}
