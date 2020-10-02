70e629d0b433fd7764eaae65a6c144a94ade2434
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-10-02 18:43:12 +0300

    some changed

--- EasyHomeWork/Laba_1_1/Indv.c
+++ EasyHomeWork/Laba_1_1/Indv.c
@@ -8,15 +8,16 @@ int main(void)
     scanf("%f",&r);
     float c = 2 * M_PI * r;
     printf("C = %.3f\n",c);
-    printf("\n\n\n");
+    printf("\n\n");
     printf("Input the r for area circle: ");
     scanf("%f",&r);
     float s = M_PI * pow(r,2);
     printf("S = %.3f\n",s);
-    printf("\n\n\n");
+    printf("\n\n");
     printf("Input the r for volume circle: ");
     scanf("%f",&r);
     float v = 4/3 * M_PI * pow(r,3);
     printf("V = %.3f\n",v);
+    printf("Create by Liashenko Rostyslav\n");
     return 0;
 }
