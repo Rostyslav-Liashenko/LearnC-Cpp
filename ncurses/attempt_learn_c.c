31f3081501a95f373ba1f32163f465b3d1ec97a5
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-10-08 20:30:34 +0300

    Learn how to create window in ncurses

--- ncurses/attempt_learn_c.c
+++ ncurses/attempt_learn_c.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <ncurses.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    initscr(); /* init ncurses */
+    if (!initscr())
+    {
+        fprintf(stderr,"Error initialising ncurses.\n");
+        exit(1);
+    }
+
+    WINDOW *win = newwin(LINES,COLS,0,0);
+    printw("Hello, world!");
+
     curs_set(0); /* cursor not visibility */
-    printw("Hello, World");
     refresh();
     getch();
+    delwin(win);
     endwin(); /* close ncurses */
     return 0;
 }
