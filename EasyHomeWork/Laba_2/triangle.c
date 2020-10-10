08f43592f9a7eb6e235b6e24c36542b7103153a0
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-10-10 13:58:06 +0300

    fix gramarry error

--- EasyHomeWork/Laba_2/triangle.c
+++ EasyHomeWork/Laba_2/triangle.c
@@ -10,7 +10,7 @@ int main(void)
     {
         if (a == b && b == c && a == c)
         {
-            printf("Trikutnuk riznostoroniy\n");
+            printf("Trikutnuk rivnostoroniy\n");
         }
         else if (a == b || b == c || a == c)
         {
