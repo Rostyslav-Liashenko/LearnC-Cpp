02604358bc048b73066c809341dd8b73f1d99657
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-11-06 11:27:44 +0200

    Added induvidual task

---
+++ EasyHomeWork/Laba_4_function/indv.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+long NSD(long a, long b)
+{
+    while (a != b)
+        if (a > b)
+            a -= b;
+        else
+            b -= a;
+    return a;
+}
+
+long NSK(long x, long y)
+{
+    return x * y / NSD(x,y);
+}
+
+int main(void)
+{
+    int count;
+    long a,b;
+    printf("Input how many number: ");
+    scanf("%d",&count);
+    if (count <= 1)
+    {
+        printf("Your NSK is undefind\n");
+    }
+    printf("Input your first number: ");
+    scanf("%ld",&a);
+    count--;
+    do
+    {
+        printf("input number: ");
+        scanf("%ld",&b);
+        a = NSK(a,b);
+        count--;
+    } while (count != 0);
+    printf("NSK your number = %ld\n",a);
+}
