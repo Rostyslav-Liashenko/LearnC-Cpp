266ca84c256debf1f045d69dc0e84ab2666ff127
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-12-07 21:12:24 +0200

    Added author

--- EasyHomeWork/Laba_7_string/part_2/indv.c
+++ EasyHomeWork/Laba_7_string/part_2/indv.c
@@ -48,12 +48,21 @@ void print_result(char *word[], int count_word, int count_space) {
     printf("%s",word[i]);
 }
 
+void print_author() {
+    printf("Create by Liashenko Rostyslav\n");
+}
+
 int main(void){
     char *word[50];
     char inp_str[61];
     printf("Input the string: ");
     fgets(inp_str,sizeof(inp_str),stdin);
     fixed_fgets(inp_str);
+    if (strlen(inp_str) > LIMIT) {
+        printf("Your string is longer");
+        print_author();
+        return 0;
+    }
     int count_symbols_without_space = count_symbols_in_string_without_space(inp_str);
     int count_word = string_to_word(inp_str,word);
     int count_need_space = (LIMIT - count_symbols_without_space) / (count_word - 1);
@@ -62,4 +71,5 @@ int main(void){
     print_result(word,count_word,count_need_space);
     putchar('.'); /* added point in result */
     putchar(10);
+    print_author();
 }
