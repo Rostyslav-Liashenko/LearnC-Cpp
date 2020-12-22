357bcdc565478edba8b9d4c5849a0bffa895c40c
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-12-22 21:21:39 +0200

    improve code for print PID and PPID

--- Standart_C/Learn_system_call/Process/test_fork_2.c
+++ Standart_C/Learn_system_call/Process/test_fork_2.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 int main(void) {
     int p = fork();
     if (p == 0) {
-        printf("I'm the child\n");
+        printf("I'm the child, PID = %d PPID = %d\n",getpid(),getppid());
     } else {
-        printf("I'm the parent\n");
+        printf("I'm the parent, PID = %d PPID = %d\n",getpid(),getppid());
     }
     return 0;
 }
