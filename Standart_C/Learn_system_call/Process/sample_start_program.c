6b8798f643997f9c224889049ac2e7ed3a16e99f
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-12-24 16:49:54 +0200

    Added code for demonstration sample start other program

---
+++ Standart_C/Learn_system_call/Process/sample_start_program.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    int pid;
+    pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(1);
+    }
+    if (pid == 0) {
+        execlp("ls","ls","-l","-a","/var", NULL);
+        perror("ls");
+        exit(1);
+    }
+    wait(NULL);
+    printf("Ok\n");
+    return 0;
+}
