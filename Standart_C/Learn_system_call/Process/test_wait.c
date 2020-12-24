29431d426f6309ffd8cedce727052f15331c8fc4
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-12-24 16:05:21 +0200

    Improve example system call wait

--- Standart_C/Learn_system_call/Process/test_wait.c
+++ Standart_C/Learn_system_call/Process/test_wait.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 int main(void) {
     int status, wr;
+    int pid_child = fork();
+    if (pid_child == 0) { /* code for children process */
+        printf("I am a children\n");
+        exit(0);
+    }
     wr = wait(&status);
     if (wr == -1) {
         printf("There are no child processes at all\n");
@@ -13,7 +19,6 @@ int main(void) {
             printf("It has exited with code = %d.\n",WEXITSTATUS(status));
         } else {
              printf("It was killed by signal %d.\n",WTERMSIG(status));
-        
         }
     }
     return 0;
