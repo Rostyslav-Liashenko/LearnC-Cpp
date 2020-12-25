95ae446a1e0b24aa64ef0ddf401aa7794b88da8e
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-12-25 17:09:40 +0200

    Learn system call time

---
+++ Standart_C/Learn_system_call/Process/test_time.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(void) {
+    
+    time_t since_epoch;
+    struct tm* loc_time;
+    since_epoch = time(NULL);
+    loc_time = localtime(&since_epoch);
+    printf("Curent year = %d\n",loc_time->tm_year + 1900);
+    printf("Current month = %d\n",loc_time->tm_mon + 1);
+    printf("Current day = %d\n",loc_time->tm_mday);
+    printf("Current hour = %d\n",loc_time->tm_hour);
+    printf("Current minunte = %d\n",loc_time->tm_min);
+    printf("Current sec = %d\n",loc_time->tm_sec);
+    printf("Since Epoch = %ld\n",since_epoch);
+    return 0;
+}
