5cac1fe779c9445935200ac788b95722f4645cfb
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-12-29 14:53:12 +0200

    Learn signal in UNIX-system

---
+++ Standart_C/Learn_system_call/Process/Signal/test_signal.c
@@ -0,0 +1,21 @@
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+volatile static sig_atomic_t n = 0;
+const char message[] = "Press it again, I like it\n";
+
+void handler(int s) {
+    int save_errno = errno;
+    signal(SIGINT,handler);
+    n++;
+    write(1,message,sizeof(message)-1);
+    errno = save_errno;
+}
+
+int main() {
+    signal(SIGINT,handler);
+    while(n < 25) {
+        sleep(1);
+    }
+    return 0;
+}
