36b9e94ce5729cfcf3ce0988cace2ed4a882a7bf
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-01-08 23:23:32 +0200

    Learn const method

--- C++/Classes/Complex.cpp
+++ C++/Classes/Complex.cpp
@@ -5,23 +5,25 @@ public:
     Complex(double a_re, double a_im)
         { re = a_re; im = a_im; }
     Complex(double a){ re = a; im = 0;} // transformation construct
-    double GetRe() { return re; }
-    double GetIm() { return im; }
-    double Modulo() { return sqrt(re*re + im*im); }
-    double Argument() { return atan2(im, re); }
-    Complex operator+(Complex op2) {
+
+    double GetRe() const { return re; }
+    double GetIm() const { return im; }
+    double Modulo() const { return sqrt(re*re + im*im); }
+    double Argument() const { return atan2(im, re); }
+
+    Complex operator+(const Complex &op2) const {
         Complex res(re + op2.re, im + op2.im);
         return res;
     }
-    Complex operator-(Complex op2) {
+    Complex operator-(const Complex &op2) const {
         Complex res(re - op2.re, im - op2.im);
         return res;
     }
-    Complex operator*(Complex op2) {
+    Complex operator*(const Complex &op2) const {
         Complex res(re * op2.re - im * op2.im, re * op2.im + im * op2.re);
         return res;
     }
-    Complex operator/(Complex op2) {
+    Complex operator/(const Complex &op2) const {
         double dvs = op2.re * op2.re + op2.im * op2.im;
         Complex res((re * op2.re + im * op2.im) / dvs, (im * op2.re - re*op2.im) / dvs);
         return res;
