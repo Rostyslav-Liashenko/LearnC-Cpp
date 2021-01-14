902f00fb468bc3d18a536a1e9bf213291b8f5ac8
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-01-14 23:00:24 +0200

    learn init member other class in construct

---
+++ C++/Classes/test_init_member_class_in_construct.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class A {
+    int x,y;
+public:
+    A(int a_x, int a_y) {
+        x = a_x;
+        y = a_y;
+    }
+   
+    int GetX() { return x; }
+    int GetY() { return y; }
+
+    void Display() {
+        std::cout << "x = " << x << std::endl;
+        std::cout << "y = " << y << std::endl;
+    }
+};
+
+class B {
+    int x;
+    A a; // class A
+public:
+    B() : a(2,3) {
+      x = 0;  
+    }
+    void Display() {
+        std::cout << "x = " << x << std::endl;
+        std::cout << "Class A" << std::endl;
+        std::cout << "a.x = " << a.GetX() << std::endl;
+        std::cout << "a.y = " << a.GetY() << std::endl;
+    }
+};
+
+int main(void) {
+    B some_variable;
+    some_variable.Display();
+    return 0;
+}
