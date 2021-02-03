8c3c1de36aa5ccf3588a4135006fb2470175822e
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-02-03 20:36:15 +0200

    Complete work

--- EasyHomeWork/Laba_9_file/part_1/indv/struct_with_text_file.c
+++ EasyHomeWork/Laba_9_file/part_1/indv/struct_with_text_file.c
@@ -33,6 +33,19 @@ void save_structs(char *file_name, struct tovar *tv, int count) {
     }
 }
 
+int load_structs(char *file_name, struct tovar *tv, int count) {
+    FILE *f;
+    if ((f = fopen(file_name,"r")) == NULL) {
+        perror("Error with file!\n");
+        return -1;
+    }
+    for (int i = 0; i < count; i++) {
+        fscanf(f,"%s%lf",&tv[i].name,&tv[i].price);
+    }
+    fclose(f);
+    return 0;
+}
+
 int main(void) {
     char file_name_out[] = {"Liashenko_out.txt"};
     struct tovar t_in[COUNT];
@@ -47,7 +60,8 @@ int main(void) {
     printf("Save struct!!!\n");
     save_structs(file_name_out,t_in,count);
     printf("Loading struct!!!\n");
-    
+    load_structs(file_name_out,t_out,count);
+    printf("Result\n");
     show_info(&t_out[0]);
     show_info(&t_out[1]);
     printf("Create By Liashenko Rostyslav PSK-18\n");
