90b7a7a197e550fc82b39b21f55a84e9b54d73e9
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-02-06 22:38:21 +0200

    Learn static method

--- C++/other/test_static_field.cpp
+++ C++/other/test_static_field.cpp
@@ -1,7 +1,13 @@
+#include <iostream>
+
 class A {
     int other_field;
 public:
     static int the_static_field; // static field
+    static int TheStaticMethod(int a, int b) {
+        std::cout << a << std::endl;
+        std::cout << b << std::endl;
+    }
 };
 
 int A::the_static_field = 0;
@@ -10,4 +16,6 @@ int main(void) {
     A a;
     a.the_static_field = 15;
     A::the_static_field = 16;
+    A::TheStaticMethod(5,15);
+    a.TheStaticMethod(10,7);
 }
