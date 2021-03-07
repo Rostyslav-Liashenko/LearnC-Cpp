9f97b4de519f1e7021d3d5af2ce70845b4e1817b
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-03-07 13:44:45 +0200

    Added example for learn upcast

---
+++ C++/Polymorphism/Shop.cpp
@@ -0,0 +1,77 @@
+// test polymorphism and upcast
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+class Product {
+protected:    
+    string name;
+    double price;
+    string producer;
+public:
+    virtual void PrintInfo() {
+        cout << "Name: " << name << endl;
+        cout << "Price: " << price << endl;
+        cout << "Producer: " << producer << endl; 
+    }
+};
+
+class User {
+    string name;
+public:
+    void SelectedProduct(Product *pr) {
+        cout << "Selected product" << endl;
+        pr->PrintInfo();
+    }
+};
+
+class Milk : public Product {
+    double volume;
+public:
+    Milk(string aname, double aprice, string aproducer, double avolume) {
+        name = aname;
+        price = aprice;
+        producer = aproducer;
+        volume = avolume;
+    }
+
+    void PrintInfo() override {
+        cout << "Name: " << name << endl;
+        cout << "Price: " << price << endl;
+        cout << "Producer: " << producer << endl;
+        cout << "Volume: " << volume << endl;
+    }
+};
+
+class Bread : public Product {
+    int specias;
+public:
+
+    Bread(string aname, double aprice, string aproducer, double aspecias) {
+        name = aname;
+        price = aprice;
+        producer = aproducer;
+        specias = aspecias;
+    }
+    void PrintInfo() override {
+    cout << "Name: " << name << endl;
+    cout << "Price: " << price << endl;
+    cout << "Producer: " << producer << endl;
+    cout << "Specias: " << specias << endl;  
+    }
+};
+
+int main(void) {
+    Milk m("Slovyanochka",20.45,"Slovanyan",0.5);
+    Bread b("Jitniy",5.45,"Zolotonosha",1);
+    User customer;
+    cout << "First product" << endl;
+    customer.SelectedProduct(&m);
+    cout << endl;
+    cout << "Second product" << endl;
+    customer.SelectedProduct(&b);   
+    return 0;
+}
