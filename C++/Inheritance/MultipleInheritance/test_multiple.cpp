e50e5cc40b930a89f26468f794c9e84c617e1d97
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-03-14 19:55:07 +0200

    Learn how work constructor child with multiple inheritance

--- C++/Inheritance/MultipleInheritance/test_multiple.cpp
+++ C++/Inheritance/MultipleInheritance/test_multiple.cpp
@@ -5,6 +5,10 @@ using std::endl;
 
 class Car {
 public:
+    Car() {
+        cout << "Call constructor Car" << endl; 
+    }
+
     void Drive() {
         cout << "I am driving..." << endl;
     }
@@ -12,13 +16,19 @@ public:
 
 class Airplane {
 public:
+    Airplane() {
+        cout << "Call constructor Airplane" << endl;
+    }
     void Fly() {
         cout << "I am flying..." << endl;
     }
 };
 
 class FlyingCar : public Car, public Airplane {
-
+public:
+    FlyingCar() {
+        cout << "Call constructor FlyingCar" << endl;
+    }
 };
 
 int main(void) {
