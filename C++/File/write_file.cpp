e167409379d4d7136963c3edf39a2caccf27d421
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-03-21 22:34:20 +0200

    Learn how write data in file

--- C++/File/write_file.cpp
+++ C++/File/write_file.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <fstream>
+
+#define COUTREPETER 5 
+
 using std::ofstream;
 using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
 
 int main(void) {
-    string path = "GONEZ_BOHDAN.txt"; 
+    string path = "Test.txt"; 
     ofstream fout;
-    fout.open(path);
+    fout.open(path,ofstream::app); // append
 
     if (!fout.is_open()) {
         std::cout << "Error, file not open" << std::endl;
         return -1;
     } 
-    fout << "Test write" << std::endl;
+    int tmp;
+    for (int i = 0; i < COUTREPETER; i++) {
+        cout << "Input the number " << i << endl;
+        cin >> tmp;
+        fout << tmp << endl;
+    }
     fout.close();
     return 0;
 }
