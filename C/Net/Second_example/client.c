b42ee3eb9ed7507b716736115ea020e3cea84884
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-04-23 21:34:59 +0300

    Added proccessing exception in client part

--- C/Net/Second_example/client.c
+++ C/Net/Second_example/client.c
@@ -14,12 +14,29 @@ int main(int argc, char *argv[]) {
     int n;
     int num;
     struct sockaddr_in serv_addr;
-    
+
+    if(argc != 2) {
+        printf("\n Usage: %s <ip of server> \n",argv[0]);
+        return 1;
+    }
     sockfd = socket(AF_INET,SOCK_STREAM,0);
+    if (sockfd < 0) {
+        printf("\n Error : Could not create socket \n");
+        return 1;
+    }
+
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(5000);
-    inet_pton(AF_INET, argv[1], &serv_addr.sin_addr);
-    connect(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
+    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
+    {
+        printf("\n inet_pton error occured\n");
+        return 1;
+    }
+    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+       printf("\n Error : Connect Failed \n");
+       return 1;
+    }
     while ( (n = read(sockfd,&num,sizeof(num))) > 0) {
         printf("%d\n",num);
     }
